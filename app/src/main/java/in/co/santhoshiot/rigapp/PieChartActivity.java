1f025e5178b6375bd8faa7500d49ff225031cac2
mathi462 <mastermathi7@gmail.com>
2019-05-09 10:15:15 +0530

    new chat

--- app/src/main/java/in/co/santhoshiot/rigapp/PieChartActivity.java
+++ app/src/main/java/in/co/santhoshiot/rigapp/PieChartActivity.java
@@ -3,14 +3,11 @@ package in.co.santhoshiot.rigapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.PieChart;
+public class PieChartActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-    PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_pie_chart);
     }
 }
