348388ed58c37c6807dc3c65d409bbf763df9e0d
santhosh1211 <santhosh10dm43@gmail.com>
2019-05-13 20:34:25 +0530

    before leave Home

--- app/src/main/java/in/co/santhoshiot/rigapp/NewReport.java
+++ app/src/main/java/in/co/santhoshiot/rigapp/NewReport.java
@@ -1,7 +1,16 @@
 package in.co.santhoshiot.rigapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class NewReport extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_report, container, false);
 
+        return view;
+    }
 }
